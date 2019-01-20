750c53c9d68d1adc3ffe25a01a6653ee1a80c595
Al Bennett <al@sensational.systems>
2019-01-20 10:16:05 +0000

    Added decoder function and readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# The Things Network decoder function for TEKTELIC sensors
+TEKTELIC Communications make small "all-in-one" LoRaWAN sensors designed for home/office use. They measure temperature, humidity, PIR activity, and more. This repo contains a decoder function to unpack the payloads of these sensors and separate out the parameters being reported. This is targetted at TTN, but with minor changes will work in other network servers or Node-RED.
+
+We sell the sensors online, here:
+ * https://connectedthings.store/products/tektelic-kona-home-sensor-pir
+ * https://connectedthings.store/products/tektelic-kona-home-sensor-base
+
+To use this:
+* create a TTN application and register your devices using the TTN console
+* in your application, choose "Payload Formats" from the navigation
+* paste the decoder function into the textarea
+
+You can test the function by pasting in this example payload: 0367008904686400FF011D
+
+This should be turned into a JSON object that looks like this:
+```
+{
+  "activity": null,
+  "battery_voltage": 2.85,
+  "bytes": "A2cAiQRoZAD/AR0=",
+  "external_input": null,
+  "humidity": 50,
+  "reed_count": null,
+  "reed_state": null,
+  "temperature": 13.700000000000001
+}
+```
+(actual data from my particularly chilly office)
+
+This code is MIT licenced, and we don't claim it to be excellent, pull requests are encouraged! This isn't yet complete, it doesn't yet support accelerometer payloads for example. Again, pull request!
---
+++ tektelic-kona-home-ttn-decoder.js
@@ -0,0 +1,80 @@
+/* 
+ * Decoder function for The Things Network to unpack the payload of TEKTELIC's All-in-One Home sensors
+ * More info on the sensors/buy online:
+ * https://connectedthings.store/products/tektelic-kona-home-sensor-pir
+ * https://connectedthings.store/products/tektelic-kona-home-sensor-base
+ * This function was created by Al Bennett at Sensational Systems - al@sensational.systems
+ */
+
+function Decoder(bytes, port) {
+
+    var params = {
+        "temperature": null,
+        "humidity": null,
+        "battery_voltage": null,
+        "activity": null,
+        "reed_state": null,
+        "reed_count": null,
+        "external_input": null,
+        "bytes": bytes
+    }
+
+    for (var i = 0; i < bytes.length; i++) {
+        // Handle temperature
+        if(0x03 === bytes[i] && 0x67 === bytes[i+1]) {
+            params.temperature = 0.1 * ((bytes[i+2] << 8) | bytes[i+3]);
+            i = i+3;
+        }
+        
+        // Handle humidity
+        if(0x04 === bytes[i] && 0x68 === bytes[i+1]) {
+            params.humidity = 0.5 * bytes[i+2];
+            i = i+2;
+        }
+        
+        // Handle battery voltage
+        if(0x00 === bytes[i] && 0xFF === bytes[i+1]) {
+            params.battery_voltage = 0.01 * ((bytes[i+2] << 8) | bytes[i+3]);
+            i = i+3;
+        }
+        
+        // Handle PIR activity
+        if(0x0A === bytes[i] && 0x00 === bytes[i+1]) {
+            params.activity = true;
+            i = i+2;
+        }
+        else if(0x0A === bytes[i] && 0xFF === bytes[i+1]) {
+            params.activity = false;
+            i = i+2;
+        }
+        
+        // Handle reed switch state
+        if(0x01 === bytes[i] && 0x00 === bytes[i+1]) {
+            if(0x00 === bytes[i+2]) {
+                params.reed_state = true;
+            } else if(0xFF === bytes[i+2]) {
+                params.reed_state = false;
+            }
+            i = i+2;
+        }
+        
+        // Handle reed switch count
+        if(0x08 === bytes[i] && 0x04 === bytes[i+1]) {
+            params.reed_count = (bytes[i+2] << 8) | bytes[i+3];
+            i = i+3;
+        }
+        
+        // Handle reed switch state
+        if(0x0E === bytes[i] && 0x00 === bytes[i+1]) {
+            if(0x00 === bytes[i+2]) {
+                params.external_input = true;
+            } else if(0xFF === bytes[i+2]) {
+                params.external_input = false;
+            }
+            i = i+2;
+        }
+    }
+
+    return params
+
+}
